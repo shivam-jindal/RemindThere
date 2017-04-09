e060d521432793f2d6e2156afa2dfda5f70a7951
Shivam <shivamjindal39@gmail.com>
2017-04-09 19:44:28 +0530

    Add functions to insert task and get all tasks from database

--- app/src/main/java/shivamjindal/remindthere/DatabaseAdapter.java
+++ app/src/main/java/shivamjindal/remindthere/DatabaseAdapter.java
@@ -1,12 +1,19 @@
 package shivamjindal.remindthere;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import static android.Manifest.permission_group.LOCATION;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import shivamjindal.remindthere.models.Task;
+import shivamjindal.remindthere.models.TasksContainer;
 
 /**
  * Created by shivam on 9/4/17.
@@ -22,11 +29,70 @@ public class DatabaseAdapter {
      *
      * @param context Context object
      */
-    public DatabaseAdapter(Context context) {
+    DatabaseAdapter(Context context) {
         helper = new DatabaseHelper(context);
         db = helper.getWritableDatabase();
     }
 
+
+    void insertTask(String title, List<String> tasks, int categoryId, boolean checkVisible) {
+        for (int i = 0; i < tasks.size(); i++) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(DatabaseHelper.TASK_TITLE, title);
+            contentValues.put(DatabaseHelper.TASK_TEXT, tasks.get(i));
+            contentValues.put(DatabaseHelper.CATEGORY_ID, categoryId);
+            contentValues.put(DatabaseHelper.CHECK_VISIBLE, checkVisible);
+
+            try {
+                db.insert(DatabaseHelper.TASK_TABLE,
+                        null,
+                        contentValues);
+                Log.i("success", "in inserting task");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    List<TasksContainer> getAllTasks() {
+        List<TasksContainer> listOfContainers = new ArrayList<>();
+        String[] columns = {
+                DatabaseHelper.TASK_TEXT,
+                DatabaseHelper.CHECK_VISIBLE,
+                DatabaseHelper.CHECKED,
+        };
+
+        Cursor cursorForCategories = db.query(true, DatabaseHelper.TASK_TABLE,
+                new String[]{DatabaseHelper.TASK_TITLE, DatabaseHelper.CATEGORY_ID},
+                null, null, null, null, null, null);
+
+        while (cursorForCategories.moveToNext()) {
+            TasksContainer newTaskContainer = new TasksContainer();
+            newTaskContainer.setTitle(cursorForCategories.getString(0));
+            newTaskContainer.setCategoryId(cursorForCategories.getInt(1));
+            List<Task> tasks = new ArrayList<>();
+            String[] selectionArgs = {String.valueOf(cursorForCategories.getInt(1))};
+            Cursor cursorForTasks = db.query(DatabaseHelper.TASK_TABLE,
+                    columns,
+                    DatabaseHelper.CATEGORY_ID + " =? ",
+                    selectionArgs,
+                    null, null, null, null);
+
+            while (cursorForTasks.moveToNext()) {
+                Task task = new Task();
+                task.setTaskName(cursorForTasks.getString(0));
+                task.setCheckVisible(cursorForTasks.getInt(1) > 0);
+                task.setChecked(cursorForTasks.getInt(2) > 0);
+                tasks.add(task);
+            }
+            cursorForTasks.close();
+            newTaskContainer.setTasks(tasks);
+            listOfContainers.add(newTaskContainer);
+        }
+        cursorForCategories.close();
+        return listOfContainers;
+    }
 
 
     private static class DatabaseHelper extends SQLiteOpenHelper {
@@ -60,8 +126,8 @@ public class DatabaseAdapter {
         public void onCreate(SQLiteDatabase db) {
             String CREATE_TASK_TABLE = "CREATE TABLE IF NOT EXISTS " + TASK_TABLE + "("
                     + UID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + CATEGORY_ID + "INTEGER AUTOINCREMENT"
-                    + TASK_TEXT + " VARCHAR(555), "
+                    + CATEGORY_ID + " INTEGER, "
+                    + TASK_TEXT + " VARCHAR(255), "
                     + TASK_TITLE + " VARCHAR(255), "
                     + CHECK_VISIBLE + " BOOLEAN DEFAULT 1, "
                     + CHECKED + " BOOLEAN DEFAULT 0 );";
