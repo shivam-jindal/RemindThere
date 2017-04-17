037ad4086bb75cbfc1be2732f8e36780d460f146
Shivam <shivamjindal39@gmail.com>
2017-04-18 05:28:43 +0530

    Added function to save check changes in database

--- app/src/main/java/shivamjindal/remindthere/InnerRVAdapter.java
+++ app/src/main/java/shivamjindal/remindthere/InnerRVAdapter.java
@@ -1,27 +1,43 @@
 package shivamjindal.remindthere;
 
 import android.content.Context;
+import android.graphics.Paint;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Handler;
+
+import shivamjindal.remindthere.models.Task;
 import shivamjindal.remindthere.models.TasksContainer;
 
 /**
  * Created by shivam on 9/4/17.
  */
 
-public class InnerRVAdapter extends RecyclerView.Adapter<InnerRVAdapter.InnerAdapterViewHolder> {
+class InnerRVAdapter extends RecyclerView.Adapter<InnerRVAdapter.InnerAdapterViewHolder> {
 
     private Context context;
     private TasksContainer taskList;
 
+    InnerRVAdapter(TasksContainer tasksList) {
+        List<Task> tasks = new ArrayList<>();
+        TasksContainer nt = new TasksContainer();
+        for (int i = 0; i < tasksList.getTasks().size(); i++) {
+            if (!tasksList.getTasks().get(i).getTaskName().equals(""))
+                tasks.add(tasksList.getTasks().get(i));
+        }
+        nt.setTasks(tasks);
+        nt.setTitle(tasksList.getTitle());
+        nt.setCategoryId(tasksList.getCategoryId());
+        this.taskList = nt;
 
-    public InnerRVAdapter(TasksContainer taskList){
-        this.taskList = taskList;
     }
 
     @Override
@@ -32,15 +48,39 @@ public class InnerRVAdapter extends RecyclerView.Adapter<InnerRVAdapter.InnerAda
     }
 
     @Override
-    public void onBindViewHolder(InnerAdapterViewHolder holder, int position) {
-        holder.taskName.setText(taskList.getTasks().get(position).getTaskName());
+    public void onBindViewHolder(final InnerAdapterViewHolder holder, int position) {
+            holder.taskName.setText(taskList.getTasks().get(position).getTaskName());
 
-        if(taskList.getTasks().get(position).isCheckVisible()){
-            holder.checkBox.setVisibility(View.VISIBLE);
-            holder.checkBox.setChecked(taskList.getTasks().get(position).isChecked());
-        }else{
-            holder.checkBox.setVisibility(View.GONE);
-        }
+            if (taskList.getTasks().get(position).isCheckVisible()) {
+                holder.checkBox.setVisibility(View.VISIBLE);
+                if(taskList.getTasks().get(position).isChecked()){
+                holder.checkBox.setChecked(true);
+                holder.taskName.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+                }
+                else{
+                    holder.checkBox.setChecked(false);
+                    holder.taskName.setPaintFlags(0);
+                }
+            } else {
+                holder.checkBox.setVisibility(View.GONE);
+            }
+
+            holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    DatabaseAdapter databaseAdapter = new DatabaseAdapter(context);
+                    databaseAdapter.changeCheck(taskList.getTasks().get(holder.getAdapterPosition()).getTaskName(), isChecked);
+                    taskList.getTasks().get(holder.getAdapterPosition()).setChecked(isChecked);
+                    android.os.Handler handler = new android.os.Handler();
+                    final Runnable r = new Runnable() {
+                        @Override
+                        public void run() {
+                            notifyItemChanged(holder.getAdapterPosition());
+                        }
+                    };
+                    handler.post(r);
+                }
+            });
     }
 
     @Override
@@ -48,7 +88,7 @@ public class InnerRVAdapter extends RecyclerView.Adapter<InnerRVAdapter.InnerAda
         if (taskList.getTasks() == null) {
             return 0;
         } else {
-            return taskList.getTasks().size();
+              return taskList.getTasks().size();
         }
     }
 
