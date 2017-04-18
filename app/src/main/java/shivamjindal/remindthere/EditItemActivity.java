d4ff120a5ad78b2c66b8a3feac417604db829449
Shivam <shivamjindal39@gmail.com>
2017-04-18 08:04:51 +0530

    Added geofences code to store new geofences

--- app/src/main/java/shivamjindal/remindthere/EditItemActivity.java
+++ app/src/main/java/shivamjindal/remindthere/EditItemActivity.java
@@ -1,9 +1,14 @@
 package shivamjindal.remindthere;
 
+import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -15,8 +20,15 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.model.LatLng;
@@ -30,7 +42,7 @@ import shivamjindal.remindthere.models.TasksContainer;
  * Created by shivam on 9/4/17.
  */
 
-public class EditItemActivity extends AppCompatActivity implements TimeReminderDialog.dialogListener, LocationReminderDialog.LocationDialogListener {
+public class EditItemActivity extends AppCompatActivity implements TimeReminderDialog.dialogListener, LocationReminderDialog.LocationDialogListener, GoogleApiClient.ConnectionCallbacks, ResultCallback<Status>, GoogleApiClient.OnConnectionFailedListener {
 
     TextView addItemButton;
     EditText title;
@@ -42,6 +54,7 @@ public class EditItemActivity extends AppCompatActivity implements TimeReminderD
 
     int PLACE_PICKET_REQUEST_CODE = 101;
     Place place;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -115,8 +128,31 @@ public class EditItemActivity extends AppCompatActivity implements TimeReminderD
                 }
             }
         });
+
+        createGoogleApi();
     }
 
+    private void createGoogleApi() {
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -183,6 +219,89 @@ public class EditItemActivity extends AppCompatActivity implements TimeReminderD
 
     @Override
     public void onLocationDialogDismiss() {
-        //AddItemActivity.startGeofence((String) place.getName(), place.getLatLng(), 100);
+        startGeofence((String) place.getName(), place.getLatLng(), 100);
+    }
+
+
+    public void startGeofence(String titleID, LatLng latLng, float radius) {
+        Geofence geofence = createGeofence(titleID, latLng, radius);
+        GeofencingRequest geofenceRequest = createGeofenceRequest(geofence);
+        addGeofence(geofenceRequest);
+    }
+
+
+    public Geofence createGeofence(String id, LatLng latLng, float radius) {
+        return new Geofence.Builder()
+                .setRequestId(id)
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                .setCircularRegion(latLng.latitude, latLng.longitude, radius)
+                .build();
+    }
+
+
+    @NonNull
+    private GeofencingRequest createGeofenceRequest(Geofence geofence) {
+        return new GeofencingRequest.Builder()
+                .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER | GeofencingRequest.INITIAL_TRIGGER_EXIT)
+                .addGeofence(geofence)
+                .build();
+    }
+
+
+    private PendingIntent geoFencePendingIntent;
+    private final int GEOFENCE_REQ_CODE = 0;
+
+
+    private PendingIntent createGeofencePendingIntent() {
+        if (geoFencePendingIntent != null)
+            return geoFencePendingIntent;
+
+        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+        return PendingIntent.getService(
+                this, GEOFENCE_REQ_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+
+    // Add the created GeofenceRequest to the device's monitoring list
+    private void addGeofence(GeofencingRequest request) {
+        if (checkPermission())
+            LocationServices.GeofencingApi.addGeofences(
+                    googleApiClient,
+                    request,
+                    createGeofencePendingIntent()
+            ).setResultCallback(this);
+    }
+
+
+    private boolean checkPermission() {
+        // Ask for permission if it wasn't granted yet
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onResult(@NonNull Status status) {
+        if (status.isSuccess()) {
+            Constants.showToast(this, "Location reminder added successfully!");
+        } else {
+            Log.e("onResult: ", String.valueOf(status));
+            // inform about fail
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
