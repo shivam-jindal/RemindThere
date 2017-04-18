c54653470864b49baf6e853fa3953ff8e9a332f8
Shivam <shivamjindal39@gmail.com>
2017-04-18 09:31:39 +0530

    Completed the functioning of alarm receiver

--- app/src/main/java/shivamjindal/remindthere/AlarmReceiver.java
+++ app/src/main/java/shivamjindal/remindthere/AlarmReceiver.java
@@ -6,9 +6,11 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.NotificationCompat;
 
@@ -19,42 +21,55 @@ import android.support.v7.app.NotificationCompat;
 public class AlarmReceiver extends BroadcastReceiver {
 
     Context context;
+    SharedPreferences sPrefs;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         this.context = context;
         String notificationText = intent.getExtras().getString("TITLE_TEXT");
         int notificationId = intent.getExtras().getInt("CATEGORY_ID");
+        sPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 
-        //CancelReminderDialog.deleteReminderOnReceive(context, notificationId);
+        Constants.deleteTimeReminderFromDB(context, notificationId);
 
         createNotification(notificationId, R.drawable.app_icon_svg, "RemindThere Reminder", notificationText);
     }
 
 
     private void createNotification(int nId, int iconRes, String title, String body) {
-        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         Drawable appIcon = ContextCompat.getDrawable(context, R.drawable.app_icon_svg);
         Intent notificationIntent = new Intent(context, MainActivity.class);
         notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
+        Uri dateUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        String notificationTone = sPrefs.getString("DATE_NOTIFICATION_URI", null);
+        if (notificationTone != null) {
+            dateUri = Uri.parse(notificationTone);
+        }
+
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent,
                 PendingIntent.FLAG_ONE_SHOT);
 
-        Notification notification = new NotificationCompat.Builder(
+        android.support.v4.app.NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(
                 context).setSmallIcon(iconRes)
                 .setLargeIcon(Constants.drawableToBitmap(appIcon))
                 .setContentTitle(title)
                 .setContentText(body)
-                .setDefaults(Notification.DEFAULT_ALL)
                 .setStyle(new NotificationCompat.BigTextStyle()
                         .bigText(body))
-                .setSound(alarmSound)
+                .setSound(dateUri)
                 .setGroup("new group")
                 .setAutoCancel(true)
                 .setColor(context.getResources().getColor(R.color.colorAccent))
-                .setContentIntent(pendingIntent)
-                .build();
+                .setContentIntent(pendingIntent);
+
+        Notification notification = notificationBuilder.build();
+        if (!sPrefs.getBoolean("DATE_VIBRATION", true)) {
+            notificationBuilder.setVibrate(null);
+        } else {
+            notification.defaults |= Notification.DEFAULT_VIBRATE;
+        }
 
         NotificationManager mNotificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
